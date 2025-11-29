9a3988a1c783ae30574df94df39693c43ed0d621
Igor <igor.lakic2@gmail.com>
2025-11-29 22:13:20 +0100

    project init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import AppHeader from "./AppHeader";
+import Content from "./Content";
+
+function App() {
+  return (
+    <div>
+      <main>
+        <AppHeader />
+        <>
+          <Content />
+        </>
+      </main>
+    </div>
+  );
+}
+
+export default App;
